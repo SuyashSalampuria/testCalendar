ab5c2357a4557786fbc650dc9cf62bba91ed61ff
Suyash <suyash546@gmail.com>
2019-08-31 12:36:01 +0530

    view updated

--- src/App.js
+++ src/App.js
@@ -4,23 +4,69 @@ import dayGridPlugin from '@fullcalendar/daygrid'
 import timeGridPlugin from '@fullcalendar/timegrid'
 import listPlugin from '@fullcalendar/list'
 import bootstrapPlugin from '@fullcalendar/bootstrap'
-
-import './App.css'
-
 import './main.scss' // webpack must be configured to do this
 
 export default class DemoApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: null,
+      finalEvents: null
+    };
+  }
+  componentDidMount() {
+    fetch("http://192.168.121.228:60015/api/placement_and_internship/calendar/?format=json")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result.results.items
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error: error
+          });
+        }
+      )
+      if(this.state.items)
+      {
+          var finalEvents=null;
+          this.state.items.forEach(element => {
+
+            finalEvents.push({
+              title:element.title,
+              start: element.startTime,
+              
+              allDay: false,
+              color:'red',
+              textColor:'black'
+
+            })
+          });
+          this.setState({
+            
+            finalEvents:finalEvents
+          });
+      }
+
+
+  }
+
+
   calendarRef = React.createRef()
   render() {
+
     return (
       <FullCalendar defaultView="dayGridMonth" 
       ref={this.calendarRef}
       weekends={true}
-      events={[
-        { title: '1st test', date: '2019-08-01' },
-        { title: '2nd test', date: '2019-08-01',textColor:'white', backgroundColor:'red' }
-      ]}
-       aspectRatio = '2'
+      events={this.state.finalEvents}
+       aspectRatio = '1.195'
       
       plugins={[ dayGridPlugin, timeGridPlugin ,listPlugin , bootstrapPlugin]}
       header={{
